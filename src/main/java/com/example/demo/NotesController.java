eea83be4731009c1edf28e4fde07de59230e454d
Benjamin Hughes <behu@kea.dk>
2021-03-03 09:52:19 +0100

    asd

--- src/main/java/com/example/demo/NotesController.java
+++ src/main/java/com/example/demo/NotesController.java
@@ -15,35 +15,40 @@ import java.util.List;
 public class NotesController {
     @PostMapping("/addNote")
     public String addNote(@RequestParam("note") String note, HttpServletRequest request) {
+        HttpSession session = request.getSession();
         //get the notes from request session
-        List<String> notes = (List<String>) request.getSession().getAttribute("notesSession");
+        List<String> notes = (List<String>) session.getAttribute("notes");
         //check if notes is present in session or not
         if (notes == null) {
             notes = new ArrayList<>();
             // if notes object is not present in session, set notes in the request session
-            request.getSession().setAttribute("notesSession", notes);
+            session.setAttribute("notes", notes);
         }
         notes.add(note);
-        //System.out.println(notes);
-        request.getSession().setAttribute("notesSession", notes);
+        session.setAttribute("notes", notes);
+
         return "redirect:/home";
     }
 
     @GetMapping("/home")
-    public String home(Model model, HttpSession session) {
-        List<String> notes = (List<String>) session.getAttribute("notesSession");
+    public String home(Model model, HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        List<String> notes = (List<String>) session.getAttribute("notes");
         if (notes == null) {
             notes = new ArrayList<>();
         }
 
         model.addAttribute("notes", notes);
+
         return "home";
     }
 
     @PostMapping("/invalidate/session")
     public String destroySession(HttpServletRequest request) {
+        HttpSession session = request.getSession();
         //invalidate the session , this will clear the data from configured database (Mysql/redis/hazelcast)
-        request.getSession().invalidate();
+        session.invalidate();
+
         return "redirect:/home";
     }
 }
